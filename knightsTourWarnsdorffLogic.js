74e7b58947c0da905d57b6ccaac6b783ea5b55a9
Nicholas Bernhard <nbernhard@cuestixint.com>
2019-12-13 15:48:35 -0700

    Developing function that checks available moves

--- knightsTourWarnsdorffLogic.js
+++ knightsTourWarnsdorffLogic.js
@@ -37,8 +37,61 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    function runTheKnightsTour() {
+    function runTheKnightsTour(startingX, startingY) {
         console.log("runProgramButton was clicked.");
+        let counter = 0;
+        // A MULTIDIMENSIONAL ARRAY.
+        // INTERNAL ARRAYS ARE ROWS.
+        // NUMBERS ARE INDIVIDUAL CELLS.
+        // 0 MEANS THE CELL HAS NEVER BEEN OCCUPIED.
+        // 1 MEANS THE CELL HAS BEEN OCCUPIED.
+        let chessBoardArray = [
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0]
+        ];
+        let currentCellX = startingX;
+        let currentCellY = startingY;
+        function checkForAvailableMoves(x, y) {
+            console.log("Y: " + y);
+            console.log("X: " + x);
+            // This function will check for available knight's moves,
+            // Moving clockwise from the top.
+            let move1 = {x: x - 1, y: y - 2};
+            let move2 = {x: x + 1, y: y - 2};
+            let move3 = {x: x + 2, y: y - 1};
+            let move4 = {x: x + 2, y: y + 1};
+            let move5 = {x: x + 1, y: y + 2};
+            let move6 = {x: x - 1, y: y + 2};
+            let move7 = {x: x - 2, y: y + 1};
+            let move8 = {x: x - 2, y: y - 1};
+
+            let arrayOfMoves = [
+                move1, move2, move3, move4,
+                move5, move6, move7, move8
+            ];
+            let possibleMovesArray = [];
+            function checkMove(moveItem, index) {
+                let yIsOnTheBoard = chessBoardArray[moveItem.y] !== undefined;
+                let xIsOnTheBoard = (yIsOnTheBoard && chessBoardArray[moveItem.y][moveItem.x] !== undefined);
+                let cellIsOnTheBoard = (yIsOnTheBoard && xIsOnTheBoard);
+                let moveIsValid = (cellIsOnTheBoard && chessBoardArray[moveItem.y][moveItem.x] === 0);
+
+                    if (moveIsValid) {
+                        console.log("POSSIBLE MOVE: ");
+                        console.log(moveItem);
+                        console.log(index);
+                        possibleMovesArray.push(moveItem);
+                    }
+            };
+            arrayOfMoves.forEach(checkMove);
+        }
+        checkForAvailableMoves(currentCellX, currentCellY);
     }
 
     // MAIN EVENT LISTENERS
@@ -49,6 +102,8 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }); 
 
-    whenClicked("runProgramButton", runTheKnightsTour);
+    whenClicked("runProgramButton", function () {
+        runTheKnightsTour(7, 4);
+    });
 
 });
